7936c29cb377748805a663bcdd0cfed65e33dff6
Piotr Grzybowski <p.grzybowski2@gmail.com>
2021-01-16 11:45:12 +0100

    Add push to Queue.\nAdd test_queue file.

--- structures/tests/test_queue.py
+++ structures/tests/test_queue.py
@@ -1,4 +1,5 @@
 from unittest import TestCase, main
+
 from structures.queues import Queue
 
 
@@ -19,6 +20,28 @@ class TestQueue(TestCase):
         self.assertEqual(len(queue), 1)
         self.assertIsNone(queue.head.next)
         self.assertIsNone(queue.tail.next)
+
+    def test_push_to_one_element_queue(self):
+        queue = Queue[int]()
+        queue.push(5)
+        queue.push(7)
+
+        self.assertEqual(queue.head.value, 5)
+        self.assertEqual(queue.tail.value, 7)
+        self.assertEqual(len(queue), 2)
+
+        self.assertEqual(queue.head.next.value, 7)
+        self.assertIsNone(queue.tail.next)
+
+    def test_push_to_many_element_queue(self):
+        queue = Queue[int]()
+        queue.push(5)
+        queue.push(7)
+        queue.push(9)
+
+        self.assertEqual(queue.head.value, 5)
+        self.assertEqual(queue.tail.value, 9)
+        self.assertEqual(len(queue), 3)
 
 
 if __name__ == '__main__':

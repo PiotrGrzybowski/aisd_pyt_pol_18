8b116dac7b0aa224b221607be2667f56eda12c38
Piotr Grzybowski <p.grzybowski2@gmail.com>
2021-01-16 14:13:01 +0100

    Add valid_parenthesis

--- tasks/valid_parenthesis.py
+++ tasks/valid_parenthesis.py
@@ -1,8 +1,31 @@
-
+from structures.queues import Stack
 
 LEFT_PARENTHESIS = '('
 RIGHT_PARENTHESIS = ')'
 
 
 def is_expression_valid(expression: str) -> bool:
-    pass
+    stack = Stack[str]()
+    for char in expression:
+        if char == LEFT_PARENTHESIS:
+            stack.push(char)
+        elif char == RIGHT_PARENTHESIS:
+            if len(stack) > 0 and stack.front() == LEFT_PARENTHESIS:
+                stack.pop()
+            else:
+                return False
+    return not len(stack) > 0
+
+
+if __name__ == '__main__':
+    case = '((()))'
+    print(f'{case}, is valid: {is_expression_valid(case)}')
+
+    case = '((()()))'
+    print(f'{case}, is valid: {is_expression_valid(case)}')
+
+    case = '((())))'
+    print(f'{case}, is valid: {is_expression_valid(case)}')
+
+    case = '((()))('
+    print(f'{case}, is valid: {is_expression_valid(case)}')

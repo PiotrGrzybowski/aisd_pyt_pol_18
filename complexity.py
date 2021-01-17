5cef64e40771799ccd2c08b0d81b86ca044b816f
Piotr Grzybowski <p.grzybowski2@gmail.com>
2021-01-17 10:20:35 +0100

    Add complexity.

--- complexity.py
+++ complexity.py
@@ -1,11 +1,15 @@
 import plotly.graph_objects as go
 import math
 
-length = 100
+length = 1000
 domain = list(range(1, length + 1))
 mode = 'lines+markers'
 
 figure = go.Figure()
 figure.add_trace(go.Scatter(x=domain, y=[x for x in domain], mode=mode, name='O(N)'))
-
+figure.add_trace(go.Scatter(x=domain, y=[x ** 2 for x in domain], mode=mode, name='O(N^2)'))
+figure.add_trace(go.Scatter(x=domain, y=[x ** 3 for x in domain], mode=mode, name='O(N^3)'))
+figure.add_trace(go.Scatter(x=domain, y=[math.log2(x) for x in domain], mode=mode, name='O(log(N))'))
+figure.add_trace(go.Scatter(x=domain, y=[x * math.log2(x) for x in domain], mode=mode, name='O(Nlog(N))'))
 figure.show()
+

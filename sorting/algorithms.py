6ee8719e058b4a68678f6c706a8d8ffd433ce5a4
Piotr Grzybowski <p.grzybowski2@gmail.com>
2021-01-17 11:16:24 +0100

    Add buble_sort signature

--- sorting/algorithms.py
+++ sorting/algorithms.py
@@ -1,21 +1,21 @@
 from typing import List
 
 
-def generate_random_list(length, start, end) -> List[int]:
+def generate_random_list(length: int, start: int, end: int) -> List[int]:
     """Zwraca listę liczb losowo uporządkowanych o długości len z zakresu
     9,4,3,111,24346,...
     """
     pass
 
 
-def generate_ordered_list(length):
+def generate_ordered_list(length: int) -> List[int]:
     """Zwraca listę wartości uporządkowanych od 0 do lenght
     0,1,2,3,4,5...,length-1
     """
     pass
 
 
-def generate_reversed_list(length):
+def generate_reversed_list(length: int) -> List[int]:
     """Zwraca listę wartości odwrotnie uporządkowanych od 0 do lenght
     length-1,length-2,length-3,length,...,0
     """

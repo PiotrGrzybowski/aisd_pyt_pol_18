3c5f8955ae78502a7a099ac49dfbc3db4ccfe866
Piotr Grzybowski <p.grzybowski2@gmail.com>
2021-01-24 13:43:31 +0100

    Add merge_intervals

---
+++ tasks/merge_intervals.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+def merge_intervals(intervals: List[List[int]]) -> List[List[int]]:
+    pass
+
+
+if __name__ == '__main__':
+    intervals = [[20, 24], [30, 31], [4, 8], [6, 10], [18, 22]]
+    print(merge_intervals(intervals))

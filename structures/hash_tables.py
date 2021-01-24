7e6fbfbdadb52ac184c1c2f8bbc7eb7879c031e3
Piotr Grzybowski <p.grzybowski2@gmail.com>
2021-01-24 11:12:18 +0100

    Add HashSet

--- structures/hash_tables.py
+++ structures/hash_tables.py
@@ -7,6 +7,12 @@ class HashSet:
 
         self.buckets = self._build_initial_buckets()
 
+    def add(self, value):
+        hash_value = hash(value)
+        bucket_index = hash_value % (len(self.buckets))
+        self.buckets[bucket_index].append(value)
+        self.size += 1
+
     def _build_initial_buckets(self):
         return [[] for _ in range(self.initial_buckets)]
 
